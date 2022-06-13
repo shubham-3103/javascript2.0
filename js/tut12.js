f7ba2922684b44774eef6dd04dedc8a3fa04bf9c
Shubham <shubhamsharma31031991@gmail.com>
2022-06-13 23:51:43 +0530

    tut12

---
+++ js/tut12.js
@@ -0,0 +1,11 @@
+console.log("Welcome to tut12");
+ let i=0;
+document.getElementById('heading').addEventListener
+('click', function(e){
+    // console.log('you have clicked the heading');
+    // console.log(e.target.className);
+    // console.log(e.target.id);
+    // location.href='//youtube.com'
+    i++;
+    console.log(i);
+});
